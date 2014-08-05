74cf7025542dbc8bab3c09008f40d66918a8d826
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2014-08-05 11:00:30 -0300

    missing file added

---
+++ Needles.Tests/Helpers/EnumerableHelperTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Needles.Helpers;
+using NUnit.Framework;
+
+namespace Needles.Tests.Helpers
+{
+    public class EnumerableHelperTest
+    {
+        [Test]
+        public void NullTest()
+        {
+            IEnumerable<int> source = null;
+
+            Assert.IsTrue(source.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            IEnumerable<int> source = new int[0];
+
+            Assert.IsTrue(source.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void NotNullOrEmptyTest()
+        {
+            IEnumerable<int> source = new[] {1};
+
+            Assert.IsFalse(source.IsNullOrEmpty());
+        }
+    }
+}
